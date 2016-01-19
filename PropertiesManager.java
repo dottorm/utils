24456c4b10d30f782ac51ba4e40a9cae3842f875
dottorm <marcoparrillo@gmail.com>
2016-01-19 12:51:55 +0100

    Create PropertiesManager.java

---
+++ PropertiesManager.java
@@ -0,0 +1,32 @@
+public class PropertiesManager {
+	
+	private static final Logger log = LogManager.getLogger(Constant.LOG_APPENDER_WS);
+	private String propFileName = "enginedpdeca.properties";
+	
+	private Properties prop = new Properties();
+	
+	private static class LazyHolder{
+		private static final PropertiesManager INSTANCE = new PropertiesManager();
+	}
+	
+	private PropertiesManager(){
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+		try{
+			prop.load(inputStream);
+		}catch(IOException e){
+			log.error("Unable to load Properties file: "+e);
+		}
+	}
+	
+	public static PropertiesManager getInstance(){
+		return LazyHolder.INSTANCE;
+	}
+	
+	public String getdataSource() throws Exception {
+		return prop.getProperty("deca.ds");
+	}
+	
+	public String getProperty(String property){
+		return prop.getProperty(property);
+	}
+}
